7c9c0ac7fc4cb5cb7a7952211282f82a696ad09a
cesarls <cesarpalsoccer@gmail.com>
2021-01-21 12:53:50 +0100

    Ejercicio 3 de la unidad 5 resuelto

---
+++ src/unidad5/Autor.java
@@ -0,0 +1,51 @@
+package unidad5;
+
+public class Autor {
+	private String nombre;
+	private String email;
+	private String genero;
+
+	// constructor
+	Autor(String nombre, String email, String genero) {
+		this.nombre = nombre;
+		this.email = email;
+		if (genero == "masculino" || genero == "femenino") {
+			this.genero = genero;
+		}
+	}
+
+	// methods Get/Set
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	// method toString
+	public String toString() {
+		return nombre + " (" + genero + ") " + email;
+	}
+
+	public static void main(String[] args) {
+		Autor reverte = new Autor("Luis Cernuda", "cernuda@gmail.com", "masculino");
+		System.out.println(reverte.getNombre());
+		System.out.println(reverte.getEmail());
+		System.out.println(reverte.getGenero());
+		reverte.setEmail("cenurda15@gmail.com");
+		System.out.println( reverte.toString() );
+		
+		
+	}
+
+}
